e3503100ee087354928a9a51e38c5ac024292b49
nodmalikov <abcmalikov@gmail.com>
2024-06-12 16:51:20 +0500

    Add statistics form and css autofill

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const elJsMenu = document.querySelector('.js-menu');
 const elJsBtn = document.querySelector('.js-btn');
+const elJsForm = document.querySelector('.js-form');
+const elResult = document.querySelector('.statistics__result');
 
 elJsBtn.addEventListener('click', function () {
     elJsMenu.classList.toggle('mobile-header__active');
+});
+
+elJsForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    
+elResult.classList.add('statistics-open')    
 });
